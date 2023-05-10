0784d3f65e5629a5d34f6f559cb3a4c606f1a394
rikofebriyan <rikofebriyan@gmail.com>
2023-05-10 14:46:02 +0700

    aktifkan email remminder ke email riko

---
+++ app/Console/Commands/SendDelayNotificationEmail.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use App\Models\Progressrepair;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+
+class SendDelayNotificationEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'emailriko';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $progressrepair = DB::table('waitingrepairs')
+            ->join('progressrepairs', 'waitingrepairs.id', '=', 'progressrepairs.form_input_id')
+            ->select(
+                'waitingrepairs.*',
+                'progressrepairs.place_of_repair',
+                'progressrepairs.analisa',
+                'progressrepairs.action',
+                'progressrepairs.plan_start_repair',
+                'progressrepairs.plan_finish_repair',
+                'progressrepairs.pic_repair',
+                'progressrepairs.actual_finish_repair',
+                'progressrepairs.plan_start_revision',
+                'progressrepairs.plan_finish_revision',
+                'progressrepairs.reason_revision',
+                'progressrepairs.id as progressid',
+                'progressrepairs.notified_at'
+            )
+            ->where('progress', '<>', 'finish')
+            ->where('progress', '<>', 'Scrap')
+            ->where('plan_finish_repair', '<', Carbon::now())
+            ->whereNull('notified_at')->get();
+
+
+
+        foreach ($progressrepair as $repair) {
+            $data = [
+                'name' => $repair->pic_repair,
+                'item_name' => $repair->item_name,
+                'item_code' => $repair->item_code,
+                'plan_finish_repair' => Carbon::parse($repair->plan_finish_repair)->format('d-M-Y'),
+                'reason' => 'Repair is delayed',
+            ];
+
+            Mail::send('emails.demo', $data, function ($message) {
+                $message->to('rikofebriyan@gmail.com', 'PE-Digitalization')
+                    ->subject('I-Mirs Delay Notification for Your Repair');
+                $message->from('pe-digitalization@outlook.com', 'PE-Digitalization');
+            });
+
+            // Mail::send('emails.demo', $data, function ($message) use ($repair) {
+            //     $message->to('pe-digitalization@taci.toyota-industries.com', 'PE-Digitalization')
+            //         ->subject('I-Mirs Delay Notification for Your Repair');
+            //     $message->from('pe-digitalization@outlook.com', 'PE-Digitalization');
+            // });
+
+            $notified = Progressrepair::find($repair->progressid);
+            $notified->notified_at = Carbon::now();
+            $notified->save();
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,12 @@
 
 namespace App\Console;
 
+use Carbon\Carbon;
+use App\Models\Progressrepair;
+use App\Console\Commands\Inspire;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +21,60 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('emailriko')->everyMinute();
+
+        // $schedule->call(function () {
+
+        //     $progressrepair = DB::table('waitingrepairs')
+        //         ->join('progressrepairs', 'waitingrepairs.id', '=', 'progressrepairs.form_input_id')
+        //         ->select(
+        //             'waitingrepairs.*',
+        //             'progressrepairs.place_of_repair',
+        //             'progressrepairs.analisa',
+        //             'progressrepairs.action',
+        //             'progressrepairs.plan_start_repair',
+        //             'progressrepairs.plan_finish_repair',
+        //             'progressrepairs.pic_repair',
+        //             'progressrepairs.actual_finish_repair',
+        //             'progressrepairs.plan_start_revision',
+        //             'progressrepairs.plan_finish_revision',
+        //             'progressrepairs.reason_revision',
+        //             'progressrepairs.id as progressid',
+        //             'progressrepairs.notified_at'
+        //         )
+        //         ->where('progress', '<>', 'finish')
+        //         ->where('progress', '<>', 'Scrap')
+        //         ->where('plan_finish_repair', '<', Carbon::now())
+        //         ->whereNull('notified_at')->get();
+
+
+
+        //     foreach ($progressrepair as $repair) {
+        //         $data = [
+        //             'name' => $repair->pic_repair,
+        //             'item_name' => $repair->item_name,
+        //             'item_code' => $repair->item_code,
+        //             'plan_finish_repair' => Carbon::parse($repair->plan_finish_repair)->format('d-M-Y'),
+        //             'reason' => 'Repair is delayed',
+        //         ];
+
+        //         Mail::send('emails.demo', $data, function ($message) use ($repair) {
+        //             $message->to('rikofebriyan@outlook.com', 'PE-Digitalization')
+        //                 ->subject('I-Mirs Delay Notification for Your Repair');
+        //             $message->from('pe-digitalization@outlook.com', 'PE-Digitalization');
+        //         });
+
+        //         Mail::send('emails.demo', $data, function ($message) use ($repair) {
+        //             $message->to('pe-digitalization@taci.toyota-industries.com', 'PE-Digitalization')
+        //                 ->subject('I-Mirs Delay Notification for Your Repair');
+        //             $message->from('pe-digitalization@outlook.com', 'PE-Digitalization');
+        //         });
+
+        //         $notified = Progressrepair::find($repair->progressid);
+        //         $notified->notified_at = Carbon::now();
+        //         $notified->save();
+        //     }
+        // })->everyMinute();
     }
 
     /**
@@ -25,7 +84,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
